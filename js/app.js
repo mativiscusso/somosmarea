e170cb12bc71d6bf0e7fead8af3032b98346f077
mativiscusso <mativiscusso@gmail.com>
2020-04-04 22:06:59 -0300

    inicializando somosmarea.com

---
+++ js/app.js
@@ -0,0 +1,11 @@
+VANTA.NET({
+   el: "#logo",
+   mouseControls: true,
+   touchControls: true,
+   minHeight: 200.00,
+   minWidth: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   color: 0x252850,
+   backgroundColor: 0x51d1f6
+ })
